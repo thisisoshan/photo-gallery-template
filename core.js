a9f76d4062d949e317e0e4b65a48d0fc3b5d6724
Oshan Shrestha <oshan_shrestha@outlook.com>
2017-03-29 17:04:35 +0545

    Transition animation optimized

--- core.js
+++ core.js
@@ -1,33 +1,23 @@
 var myVar;
 var count;
 count=1;
-var x;
 function changeImage() {
-	$("#wrapper").removeClass(x);
     count++;
     if (count > 4) {
     	count=1;
     };
-    if (count == 1){
-    	x="fadeInDown";
-    }else if(count == 2){
-    	x="bounceIn";
-    }else if (count ==3) {
-    	x="zoomIn";
-    }else if(count ==4){
-    	x="slideInUp";
-    }
     setImage();
 }
-$(document).ready(
-	function myFunction() {
-    	myVar = setInterval(changeImage, 6000);
-	}
-);
 function setImage() {
-	document.getElementById("wrapper").src = "img/img-"+count+".jpg";
-	$("#wrapper").addClass(x);
+    document.getElementById("wrapper").src= "img/img-" + count+ ".jpg";
+	$("#wrapper").toggleClass("pulseE pulseO");
 }
 window.onload = function(){
 	$(".container").css("opacity", "1.0");
+    $("#wrapper").addClass("animated pulseE");
 };
+$(document).ready(
+    function myFunction() {
+        myVar = setInterval(changeImage, 4000);
+    }
+);
