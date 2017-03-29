c7b85c5aed21021bf389585e8c57f5bf60588f70
Oshan Shrestha <oshan_shrestha@outlook.com>
2017-03-29 22:01:17 +0545

    Some bugs fixed...

--- core.js
+++ core.js
@@ -1,23 +1,26 @@
 var myVar;
-var count;
-count=1;
+var count=1;
 function changeImage() {
     count++;
     if (count > 4) {
     	count=1;
     };
-    setImage();
+    $("#wrapper").animate(setImage(count), {
+        duration: 3200,
+        easing: "ease-in"
+    });
 }
-function setImage() {
-    document.getElementById("wrapper").src= "img/img-" + count+ ".jpg";
+function setImage(photoID) {
+    document.getElementById("wrapper").src= "img/img-" + photoID + ".jpg";
 	$("#wrapper").toggleClass("pulseE pulseO");
 }
-window.onload = function(){
+window.onload = function(e){
+    e.preventDefault();
 	$(".container").css("opacity", "1.0");
     $("#wrapper").addClass("animated pulseE");
 };
 $(document).ready(
     function myFunction() {
-        myVar = setInterval(changeImage, 4000);
+        myVar = setInterval(changeImage, 3400);
     }
 );
